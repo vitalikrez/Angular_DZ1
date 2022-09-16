c444948457006f46cd770f88b7e7578e4d35a855
vitalikrez <105062887+vitalikrez@users.noreply.github.com>
2022-09-16 21:12:14 +0300

    Update photo.component.ts

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoComponent implements OnInit {
 
-  photo = 'https://s1.stc.all.kpcdn.net/putevoditel/projectid_346574/images/tild6137-3962-4162-a534-346433623839__960.jpg'
+  photo = 'https://api.quasa.io/storage/photos/shares/00-%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%203.jpg'
   getPhoto() {
     return this.photo;
   } 
